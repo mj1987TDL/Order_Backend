1ee647dd75b65f280e1cf9a4a3d315f938fef599
Jadgoudar, Mallappa (mj330x) <mj330x@intl.att.com>
2020-03-11 13:36:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,38 +7,6 @@ const path = require('path');
 const env = process.env.NODE_ENV || 'development';
 const logDir = '../../logs';
 
-// Create the log directory if it does not exist
-if (!fs.existsSync(logDir)) {
-    fs.mkdirSync(logDir);
-}
-
-const dailyRotateFileTransport = new transports.DailyRotateFile({
-    filename: `${logDir}/%DATE%-results.log`,
-    datePattern: 'YYYY-MM-DD'
-});
-
-const logger = createLogger({
-    // change level if in dev environment versus production
-    level: env === 'development' ? 'verbose' : 'info',
-    format: format.combine(
-        format.timestamp({
-            format: 'YYYY-MM-DD HH:mm:ss'
-        }),
-        format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
-    ),
-    transports: [
-        new transports.Console({
-            level: 'info',
-            format: format.combine(
-                format.colorize(),
-                format.printf(
-                    info => `${info.timestamp} ${info.level}: ${info.message}`
-                )
-            )
-        }),
-        dailyRotateFileTransport
-    ]
-});
 
 const express = require('express');
 var app = express();
